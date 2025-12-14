3616d9bfab12716be1632f064cb4fa0efa1729d9
dloomz <dolapo20okuboyejo@gmail.com>
2025-12-14 15:37:42 +0000

    -clean up of misc things

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonpink.efeele.dev",
+  site: "https://dolapo.net/",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
